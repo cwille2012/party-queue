4ab46f5b9817ea0bb609ef38455f6bfd71d71cdd
Chris Wille <chris@christopherwille.com>
2018-09-30 08:44:33 -0400

    Format

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -133,54 +133,54 @@ class Spotify extends React.Component {
       var songList = (
         <div id="pricing-table" class="clear">
           <div class="plan">
-              <h3>Enterprise<span>$59</span></h3>
-              <a class="signup" href="">Sign up</a>         
-              <ul>
-                  <li><b>10GB</b> Disk Space</li>
-                  <li><b>100GB</b> Monthly Bandwidth</li>
-                  <li><b>20</b> Email Accounts</li>
-            <li><b>Unlimited</b> subdomains</li>			
-              </ul> 
+            <h3>Previous<span>$59</span></h3>
+            <a class="signup" href="">Sign up</a>         
+            <ul>
+              <li><b>Title</b> Disk Space</li>
+              <li><b>Artist</b> Monthly Bandwidth</li>
+              <li><b>Votes</b> Email Accounts</li>
+              <li><b>Unlimited</b> subdomains</li>			
+            </ul> 
           </div>
           <div class="plan" id="most-popular">
-              <h3>Professional<span>$29</span></h3>
-              <a class="signup" href="">Sign up</a>        
-              <ul>
-                  <li><b>5GB</b> Disk Space</li>
-                  <li><b>50GB</b> Monthly Bandwidth</li>
-                  <li><b>10</b> Email Accounts</li>
-            <li><b>Unlimited</b> subdomains</li>			
-              </ul>    
+            <h3>Now Playing<span>$29</span></h3>
+            <a class="signup" href="">Sign up</a>        
+            <ul>
+              <li><b>5GB</b> Disk Space</li>
+              <li><b>50GB</b> Monthly Bandwidth</li>
+              <li><b>10</b> Email Accounts</li>
+              <li><b>Unlimited</b> subdomains</li>
+            </ul>    
           </div>
           <div class="plan">
-              <h3>Standard<span>$17</span></h3>
-          <a class="signup" href="">Sign up</a>
-              <ul>
-                  <li><b>3GB</b> Disk Space</li>
-                  <li><b>25GB</b> Monthly Bandwidth</li>
-                  <li><b>5</b> Email Accounts</li>
-            <li><b>Unlimited</b> subdomains</li>			
-              </ul>
+            <h3>Upcoming<span>$17</span></h3>
+            <a class="signup" href="">Sign up</a>
+            <ul>
+              <li><b>3GB</b> Disk Space</li>
+              <li><b>25GB</b> Monthly Bandwidth</li>
+              <li><b>5</b> Email Accounts</li>
+              <li><b>Unlimited</b> subdomains</li>			
+            </ul>
           </div>
           <div class="plan">
-              <h3>Basic<span>$9</span></h3>
-              <a class="signup" href="">Sign up</a>		
-              <ul>
-                  <li><b>1GB</b> Disk Space</li>
-                  <li><b>10GB</b> Monthly Bandwidth</li>
-                  <li><b>2</b> Email Accounts</li>
-            <li><b>Unlimited</b> subdomains</li>			
-              </ul>
+            <h3>Upcoming<span>$9</span></h3>
+            <a class="signup" href="">Sign up</a>		
+            <ul>
+              <li><b>1GB</b> Disk Space</li>
+              <li><b>10GB</b> Monthly Bandwidth</li>
+              <li><b>2</b> Email Accounts</li>
+              <li><b>Unlimited</b> subdomains</li>			
+            </ul>
           </div> 	
           <div class="plan">
-              <h3>Basic<span>$9</span></h3>
-              <a class="signup" href="">Sign up</a>		
-              <ul>
-                  <li><b>1GB</b> Disk Space</li>
-                  <li><b>10GB</b> Monthly Bandwidth</li>
-                  <li><b>2</b> Email Accounts</li>
-            <li><b>Unlimited</b> subdomains</li>			
-              </ul>
+            <h3>Basic<span>$9</span></h3>
+            <a class="signup" href="">Sign up</a>		
+            <ul>
+              <li><b>1GB</b> Disk Space</li>
+              <li><b>10GB</b> Monthly Bandwidth</li>
+              <li><b>2</b> Email Accounts</li>
+              <li><b>Upcoming</b> subdomains</li>
+            </ul>
           </div> 	
       </div>
       );
