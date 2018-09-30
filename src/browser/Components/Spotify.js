861e948b74757a40f0eb049e590be8cc552eae2a
Chris Wille <chris@christopherwille.com>
2018-09-30 08:49:57 -0400

    HTML update

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -133,53 +133,43 @@ class Spotify extends React.Component {
       var songList = (
         <div id="pricing-table" class="clear">
           <div class="plan">
-            <h3>Previous<span>$59</span></h3>
-            <a class="signup" href="">Sign up</a>         
+            <h3>Previous<span>$59</span></h3>       
             <ul>
-              <li><b>Title</b> Disk Space</li>
-              <li><b>Artist</b> Monthly Bandwidth</li>
-              <li><b>Votes</b> Email Accounts</li>
-              <li><b>Unlimited</b> subdomains</li>			
+              <li><b>Title</b> Track Title</li>
+              <li><b>Artist</b> Track Artists</li>
+              <li><b>Votes</b> +5</li>			
             </ul> 
           </div>
           <div class="plan" id="most-popular">
-            <h3>Now Playing<span>$29</span></h3>
-            <a class="signup" href="">Sign up</a>        
+            <h3>Now Playing<span>$29</span></h3>      
             <ul>
-              <li><b>5GB</b> Disk Space</li>
-              <li><b>50GB</b> Monthly Bandwidth</li>
-              <li><b>10</b> Email Accounts</li>
-              <li><b>Unlimited</b> subdomains</li>
+            <li><b>Title</b> Track Title</li>
+            <li><b>Artist</b> Track Artists</li>
+            <li><b>Votes</b> +7</li>
             </ul>    
           </div>
           <div class="plan">
             <h3>Upcoming<span>$17</span></h3>
-            <a class="signup" href="">Sign up</a>
             <ul>
-              <li><b>3GB</b> Disk Space</li>
-              <li><b>25GB</b> Monthly Bandwidth</li>
-              <li><b>5</b> Email Accounts</li>
-              <li><b>Unlimited</b> subdomains</li>			
+              <li><b>Title</b> Track Title</li>
+              <li><b>Artist</b> Track Artists</li>
+              <li><b>Votes</b> +5</li>
             </ul>
           </div>
           <div class="plan">
-            <h3>Upcoming<span>$9</span></h3>
-            <a class="signup" href="">Sign up</a>		
+            <h3>Upcoming<span>$9</span></h3>	
             <ul>
-              <li><b>1GB</b> Disk Space</li>
-              <li><b>10GB</b> Monthly Bandwidth</li>
-              <li><b>2</b> Email Accounts</li>
-              <li><b>Unlimited</b> subdomains</li>			
+              <li><b>Title</b> Track Title</li>
+              <li><b>Artist</b> Track Artists</li>
+              <li><b>Votes</b> +2</li>		
             </ul>
           </div> 	
           <div class="plan">
-            <h3>Basic<span>$9</span></h3>
-            <a class="signup" href="">Sign up</a>		
+            <h3>Upcoming<span>$9</span></h3>	
             <ul>
-              <li><b>1GB</b> Disk Space</li>
-              <li><b>10GB</b> Monthly Bandwidth</li>
-              <li><b>2</b> Email Accounts</li>
-              <li><b>Upcoming</b> subdomains</li>
+              <li><b>Title</b> Track Title</li>
+              <li><b>Artist</b> Track Artists</li>
+              <li><b>Votes</b> +1</li>
             </ul>
           </div> 	
       </div>
