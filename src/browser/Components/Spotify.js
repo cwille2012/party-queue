15cfff827b9aae3cf1452acd96566110e9ecf7af
Chris Wille <chris@christopherwille.com>
2018-09-30 13:51:22 -0400

    remove

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -360,7 +360,7 @@ class Spotify extends React.Component {
         songContainer = (
           
             <div className="main-wrapper" style={{height:'100%'}}>
-            <div>
+            
               <div className="now-playing__img">
                 <img src={songDetails.item.album.images[0].url} />
               </div>
@@ -375,8 +375,8 @@ class Spotify extends React.Component {
                   {playButton}
                   <FontAwesomeIcon className="icon" icon={faArrowAltCircleRight} style={{fontSize:'85px'}} onClick={this.skip} />
                 </div>
-                </div>
               </div>
+              
               {sessionTable}
             </div>
             
