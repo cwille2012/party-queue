31e455baee6a374c4c9759b1a7cb8474a54ef301
Chris Wille <chris@christopherwille.com>
2018-09-30 01:18:36 -0400

    Source add

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 var spotifyStyle = require('../styles/spotify.css');
-require('./spotify-player')
+//require('./spotify-player')
 
 class Spotify extends React.Component {
   constructor(props) {
