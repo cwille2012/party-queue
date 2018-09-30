f19ab65a7f3421168fda98c638806a601683ccf5
Chris Wille <chris@christopherwille.com>
2018-09-30 10:33:41 -0400

    push

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -131,48 +131,141 @@ class Spotify extends React.Component {
     if (this.state.partyDisplay == true) {
 
       var songList = (
-        <div id="pricing-table" class="clear">
-          <div class="plan">
-            <h3>Previous<span>$59</span></h3>       
-            <ul>
-              <li><b>Title</b> Track Title</li>
-              <li><b>Artist</b> Track Artists</li>
-              <li><b>Votes</b> +5</li>			
-            </ul> 
+        <div class="limiter">
+          <div class="container-table100">
+            <div class="wrap-table100">
+              <div class="table100 ver1">
+                <div class="table100-firstcol">
+                  <table>
+                    <thead>
+                      <tr class="row100 head">
+                        <th class="cell100 column1">Employees</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Brandon Green</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Kathy Daniels</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Elizabeth Alvarado</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Michael Coleman</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Jason Cox</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Christian Perkins</td>
+                      </tr>
+
+                      <tr class="row100 body">
+                        <td class="cell100 column1">Emily Wheeler</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                
+                <div class="wrap-table100-nextcols js-pscroll">
+                  <div class="table100-nextcols">
+                    <table>
+                      <thead>
+                        <tr class="row100 head">
+                          <th class="cell100 column2">Position</th>
+                          <th class="cell100 column3">Start date</th>
+                          <th class="cell100 column4">Last Activity</th>
+                          <th class="cell100 column5">Contacts</th>
+                          <th class="cell100 column6">Age</th>
+                          <th class="cell100 column7">Address</th>
+                          <th class="cell100 column8">Card No</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="row100 body">
+                          <td class="cell100 column2">CMO</td>
+                          <td class="cell100 column3">16 Nov 2012</td>
+                          <td class="cell100 column4">16 Nov 2017</td>
+                          <td class="cell100 column5">brandon94@example.com</td>
+                          <td class="cell100 column6">30</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx6262</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">Marketing</td>
+                          <td class="cell100 column3">16 Nov 2015</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">kathy_82@example.com</td>
+                          <td class="cell100 column6">26</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx1616</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">CFO</td>
+                          <td class="cell100 column3">16 Nov 2013</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">elizabeth82@example.com</td>
+                          <td class="cell100 column6">32</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx5326</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">Designer</td>
+                          <td class="cell100 column3">16 Nov 2013</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">michael94@example.com</td>
+                          <td class="cell100 column6">22</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx6328</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">Developer</td>
+                          <td class="cell100 column3">16 Nov 2017</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">jasoncox@example.com</td>
+                          <td class="cell100 column6">25</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx7648</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">Sale</td>
+                          <td class="cell100 column3">16 Nov 2016</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">christian_83@example.com</td>
+                          <td class="cell100 column6">28</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx4152</td>
+                        </tr>
+
+                        <tr class="row100 body">
+                          <td class="cell100 column2">Support</td>
+                          <td class="cell100 column3">16 Nov 2013</td>
+                          <td class="cell100 column4">30 Nov 2017</td>
+                          <td class="cell100 column5">emily90@example.com</td>
+                          <td class="cell100 column6">24</td>
+                          <td class="cell100 column7">New York City, NY</td>
+                          <td class="cell100 column8">424242xxxxxx6668</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="plan" id="most-popular">
-            <h3>Now Playing<span>$29</span></h3>      
-            <ul>
-            <li><b>Title</b> Track Title</li>
-            <li><b>Artist</b> Track Artists</li>
-            <li><b>Votes</b> +7</li>
-            </ul>    
-          </div>
-          <div class="plan">
-            <h3>Upcoming<span>$17</span></h3>
-            <ul>
-              <li><b>Title</b> Track Title</li>
-              <li><b>Artist</b> Track Artists</li>
-              <li><b>Votes</b> +5</li>
-            </ul>
-          </div>
-          <div class="plan">
-            <h3>Upcoming<span>$9</span></h3>	
-            <ul>
-              <li><b>Title</b> Track Title</li>
-              <li><b>Artist</b> Track Artists</li>
-              <li><b>Votes</b> +2</li>		
-            </ul>
-          </div> 	
-          <div class="plan">
-            <h3>Upcoming<span>$9</span></h3>	
-            <ul>
-              <li><b>Title</b> Track Title</li>
-              <li><b>Artist</b> Track Artists</li>
-              <li><b>Votes</b> +1</li>
-            </ul>
-          </div> 	
-      </div>
+        </div>
       );
 
       var rightSideBar = (
