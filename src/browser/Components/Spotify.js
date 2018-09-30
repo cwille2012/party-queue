b35a44cbf3edf59d61b716789ec13a3b15b5ea93
Chris Wille <chris@christopherwille.com>
2018-09-30 14:06:56 -0400

    add number

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -361,6 +361,7 @@ class Spotify extends React.Component {
           
             <div className="main-wrapper" style={{height:'100%'}}>
             <div style={{display: 'inlineBlock', marginTop: '100px'}}>
+            <h2>(701) 453-6969</h2>
               <div className="now-playing__img">
                 <img src={songDetails.item.album.images[0].url} />
               </div>
