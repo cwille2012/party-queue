750a874d431be90681ff4982918833861c616f01
Chris Wille <chris@christopherwille.com>
2018-09-30 13:59:21 -0400

    push

--- src/browser/Components/Spotify.js
+++ src/browser/Components/Spotify.js
@@ -360,7 +360,7 @@ class Spotify extends React.Component {
         songContainer = (
           
             <div className="main-wrapper" style={{height:'100%'}}>
-            
+            <div>
               <div className="now-playing__img">
                 <img src={songDetails.item.album.images[0].url} />
               </div>
@@ -374,6 +374,7 @@ class Spotify extends React.Component {
                 <div className="icon-holder">
                   {playButton}
                   <FontAwesomeIcon className="icon" icon={faArrowAltCircleRight} style={{fontSize:'85px'}} onClick={this.skip} />
+                </div>
                 </div>
               </div>
               
